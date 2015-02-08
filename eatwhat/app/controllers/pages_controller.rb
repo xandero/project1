56248b56686170b12f6e119721344dd9f3886512
xandero <alexander.ohan@gmail.com>
2015-02-08 15:14:40 +1100

    hopefully this undoes my branch delete fuck up

--- eatwhat/app/controllers/pages_controller.rb
+++ eatwhat/app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @user = User.find session[:id] if session[:id].present?
+    @user = User.find session[:user_id] if session[:user_id].present?
   end
 end
