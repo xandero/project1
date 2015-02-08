db166cfffac4cc3fea9c64a4fd8ce6e783298971
xandero <alexander.ohan@gmail.com>
2015-02-08 15:04:57 +1100

    link to edit profile works

--- eatwhat/app/controllers/pages_controller.rb
+++ eatwhat/app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @user = User.find session[:user_id] if session[:user_id].present?
+    @user = User.find session[:id] if session[:id].present?
   end
 end
